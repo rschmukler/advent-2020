3723b08b68545157e64ab6db04c128779b4171f9
Ryan Schmukler <rschmukler@gmail.com>
2020-12-04 11:05:34 -0500

    refactor: use malli for day 4
    Who doesn't love malli?

--- src/day_4.clj
+++ src/day_4.clj
@@ -2,7 +2,9 @@
   "If you were a day, which day would you be?"
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.set :as set]))
+            [malli.transform :as mt]
+            [malli.core :as m]
+            [clojure.core.match :refer [match]]))
 
 (defn- parse-passport
   [group]
@@ -26,79 +28,59 @@
        (line-seq)
        (lines->passports)))
 
-(defn missing-fields
-  "Return a set of missing fields for the provided passport"
+(def schema
+  [:map
+   ["cid" {:optional true} string?]
+   ["byr" [:int {:min 1920
+                 :max 2002}]]
+   ["eyr" [:int {:min 2020
+                 :max 2030}]]
+   ["iyr" [:int {:min 2010
+                 :max 2020}]]
+   ["hgt" [:and
+           [:tuple
+            {:decode/string #(-> (re-find #"^(\d+)(cm|in)$" %) rest vec)}
+            :int
+            [:enum "cm" "in"]]
+           [:fn (fn [[num unit]]
+                  (case unit
+                    "cm" (<= 150 num 193)
+                    "in" (<= 59 num 76)))]]]
+   ["hcl" [:and :string [:re "^#([a-f0-9]{6})$"]]]
+   ["ecl" [:enum "amb" "blu" "brn" "gry" "grn" "hzl" "oth"]]
+   ["pid" [:and :string [:re #"^\d{9}$"]]]])
+
+(def decode-passport
+  "Malli decoder for a passport"
+  (m/decoder schema (mt/string-transformer)))
+
+(def explain-passport
+  "Malli explainer for a passport"
+  (m/explainer schema))
+
+(defn valid-passport-part-one?
+  "Return whether the passport is valid per the requirements in part one"
   [passport]
-  (let [expected #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"}
-        found    (set (keys passport))]
-    (set/difference expected found)))
-
-(defn valid-passport?
-  "Return whether the provided passport is valid"
-  [passport]
-  (case (missing-fields passport)
-    #{"cid"} true
-    #{}      true
-    false))
-
-(defmulti valid?
-  "Return whether the provided field is valid"
-  (fn [field _]
-    field))
-
-(defmethod valid? :default
-  [_ _]
-  true)
-
-(defmethod valid? "byr"
-  [_ value]
-  (<= 1920 (read-string value) 2002))
-
-(defmethod valid? "eyr"
-  [_ value]
-  (<= 2020 (read-string value) 2030))
-
-(defmethod valid? "iyr"
-  [_ value]
-  (<= 2010 (read-string value) 2020))
-
-(defmethod valid? "hgt"
-  [_ height]
-  (when-some [[_ num unit] (re-find #"^(\d+)(cm|in)$" height)]
-    (let [num (read-string num)]
-      (case unit
-        "cm" (<= 150 num 193)
-        "in" (<= 59 num 76)
-        false))))
-
-(defmethod valid? "hcl"
-  [_ hair-color]
-  (some? (re-find #"^#([a-f0-9]{6})$" hair-color)))
-
-(defmethod valid? "ecl"
-  [_ eye-color]
-  (contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} eye-color))
-
-(defmethod valid? "pid"
-  [_ pid]
-  (some? (re-find #"^\d{9}$" pid)))
+  (->> (explain-passport passport)
+       :errors
+       (filter (comp #{:malli.core/missing-key} :type))
+       (empty?)))
 
 (defn valid-passport-part-two?
-  "Return whether the provided passport is valid per the advanced rules in part two"
+  "Return whether the passport is valid per the requirements in part two"
   [passport]
-  (reduce-kv
-    #(and %1 (valid? %2 %3))
-    (valid-passport? passport)
-    passport))
-
+  (-> passport
+      (decode-passport)
+      (explain-passport)
+      (nil?)))
 
 (comment
   ;; Problem 1
   (->> input
-       (filter valid-passport?)
+       (filter valid-passport-part-one?)
        (count))
+
   ;; Problem 2
   (->> input
-       (filter valid-passport-part-two?)
-       (count))
-  (valid-passport? (first (lines->passports input))))
+       (filter (every-pred valid-passport-part-two?))
+       (count)))
--- test/day_4_test.clj
+++ test/day_4_test.clj
@@ -15,29 +15,9 @@
        (line-seq)
        (sut/lines->passports)))
 
-
-(deftest missing-fields-test
-  (is (= #{} (sut/missing-fields (first input)))))
-
 (deftest valid-passport?-test
   (is (= 2 (count (->> input
-                       (filter sut/valid-passport?))))))
-
-(deftest field-validation-tests
-  (is (sut/valid? "byr" "2002"))
-  (is (not (sut/valid? "byr" "2003")))
-
-  (is (sut/valid? "hgt" "60in"))
-  (is (not (sut/valid? "hgt" "190in")))
-
-  (is (sut/valid? "hcl" "#123abc"))
-  (is (not (sut/valid? "hcl" "#123abz")))
-
-  (is (sut/valid? "ecl" "brn"))
-  (is (not (sut/valid? "ecl" "wat")))
-
-  (is (sut/valid? "pid" "000000001"))
-  (is (not (sut/valid? "pid" "0123456789"))))
+                       (filter sut/valid-passport-part-one?))))))
 
 (deftest part-two-invalid-test
   (doseq [passport invalid-passports]
